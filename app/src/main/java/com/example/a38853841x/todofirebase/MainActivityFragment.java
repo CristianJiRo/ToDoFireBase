d2060901be367c6940037ec49a7c08f3f55a7ce5
CristianJiRo <38853841x@iespoblenou.org>
2017-01-31 23:10:22 +0100

    Implementacion Firebase

--- app/src/main/java/com/example/a38853841x/todofirebase/MainActivityFragment.java
+++ app/src/main/java/com/example/a38853841x/todofirebase/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.a38853841x.todofirebase;
 
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,9 +11,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import android.util.Log;
+import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,7 +28,9 @@ import java.util.Arrays;
 public class MainActivityFragment extends Fragment {
 
     private ArrayList<String> lista;
-    private ArrayAdapter<String> adapter;
+    //private ArrayAdapter<String> adapter;
+    FirebaseListAdapter<String> adapter;
+
     private static final int RC_SIGN_IN = 123;
     private FirebaseAuth auth;
 
@@ -35,6 +42,9 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference todoRef = database.getReference("ToDoFireBase");
+
         Button send = (Button) view.findViewById(R.id.bt_send);
 
         final EditText item = (EditText) view.findViewById(R.id.et_item);
@@ -43,12 +53,23 @@ public class MainActivityFragment extends Fragment {
         setupAuth();
 
         lista = new ArrayList<>();
-        adapter = new ArrayAdapter<>(
-                getContext(),
-                R.layout.item_celda,
-                R.id.tv_Item,
-                lista
-        );
+//        adapter = new ArrayAdapter<>(
+//                getContext(),
+//                R.layout.item_celda,
+//                R.id.tv_Item,
+//                lista
+//        );
+
+        adapter = new FirebaseListAdapter<String>(
+                (Activity) getContext(), String.class, R.layout.item_celda, todoRef)
+        {
+            @Override
+            protected void populateView(View v, String model, int position) {
+
+                ((TextView) v.findViewById(R.id.tv_Item)).setText(model);
+
+            }
+        };
 
         lv_ToDo.setAdapter(adapter);
 
@@ -57,8 +78,16 @@ public class MainActivityFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                adapter.add(item.getText().toString());
-                item.setText("");
+               // adapter.add(item.getText().toString());
+               // item.setText("");
+
+                String text = item.getText().toString();
+                if (text.equals("")){
+
+                    DatabaseReference newReference = todoRef.push();
+                    newReference.setValue(text);
+                    item.setText("");
+                }
 
             }
         });
